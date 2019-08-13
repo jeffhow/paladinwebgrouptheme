bfd51e9eedb33e621e99ef09bfcc9cb170f7190b
Jeff LeBoeuf <jeff@jeff.how>
2019-08-13 14:36:57 -0400

    custom logo on login page

--- functions.php
+++ functions.php
@@ -240,3 +240,20 @@ add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
 // Wordpress places empty p tags between the_content()
 // https://wordpress.stackexchange.com/questions/13798/remove-empty-paragraphs-from-the-content
 remove_filter('the_content', 'wpautop');
+
+/**
+ * Custom login page
+ */
+function my_login_logo() { ?>
+    <style type="text/css">
+        #login h1 a, .login h1 a {
+            background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/images/paladin-logo.png);
+		height:85px;
+		width:300px;
+		background-size: 300px auto;
+		background-repeat: no-repeat;
+        	padding-bottom: 30px;
+        }
+    </style>
+<?php }
+add_action( 'login_enqueue_scripts', 'my_login_logo' );
